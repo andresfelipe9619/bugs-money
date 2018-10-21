8c8d121b9a8378d83e26a5dfd6efd91095d5bd07
Andrés Suárez <andresfelipe9619@gmail.com>
2018-10-21 09:39:35 -0500

    --amend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,35 +1,18 @@
-// import superagentPromise from 'superagent-promise';
-// import _superagent from 'superagent';
-
-// const superagent = superagentPromise(_superagent, global.Promise);
-
-// proxy localhost:5000
-const API_ROOT = '/api/';
-
-const responseBody = (res) => res.body;
-
-// const requests = {
-//   del: (url) => superagent.del(`${API_ROOT}${url}`).then(responseBody),
-//   get: (url) => superagent.get(`${API_ROOT}${url}`).then(responseBody),
-//   put: (url, body) =>
-//     superagent.put(`${API_ROOT}${url}`, body).then(responseBody),
-//   post: (url, body) =>
-//     superagent.post(`${API_ROOT}${url}`, body).then(responseBody),
-// };
+const API_ROOT = "/api/";
 
 function callApi(endpoint) {
   const fullUrl =
     endpoint.indexOf(API_ROOT) === -1 ? API_ROOT + endpoint : endpoint;
 
   return fetch(fullUrl)
-      .then((response) => {
-        if (!response.ok) {
-          return Promise.reject(response.statusText);
-        }
-        return response;
-      })
-      .then((response) => response.json())
-      .catch((error) => ({error: error.message || 'Something bad happened'}));
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(response.statusText);
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .catch(error => ({ error: error.message || "Something bad happened" }));
 }
 
 // api services
