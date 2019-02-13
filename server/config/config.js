26248e1b332246e97cddc7cd7cbfb4b26a99ad6f
andresfelipe9619 <andresfelipe9619@gmail.com>
2019-02-13 07:50:39 -0500

    Change the token's expiration time

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // 24 horas
 // 30 dias
 
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '48h';
 
 // ===================
 // SEED(Semilla) de autenticacion
