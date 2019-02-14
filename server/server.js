0b6ae72e3994b1b0d48e8ed05a0574d16992b9c0
andresfelipe9619 <andresfelipe9619@gmail.com>
2019-02-13 20:03:19 -0500

    Come back to http

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const https = require('https');
+// const https = require('https');
+const http = require('http');
 const app = require('./app');
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const PORT = process.env.PORT;
-const certOptions = {
-  key: fs.readFileSync(path.resolve('cert/server.key')),
-  cert: fs.readFileSync(path.resolve('cert/server.crt')),
-};
-const server = https.createServer(certOptions, app);
+// const certOptions = {
+//   key: fs.readFileSync(path.resolve('cert/server.key')),
+//   cert: fs.readFileSync(path.resolve('cert/server.crt')),
+// };
+const server = http.createServer(app);
 
 server.listen(PORT, (err) => {
   err
