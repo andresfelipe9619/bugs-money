2545fc24a644197f1476f969de69593f81da9962
andresfelipe9619 <andresfelipe9619@gmail.com>
2019-03-16 08:12:20 -0500

    Make Dockerfile for production

--- server/app.js
+++ server/app.js
@@ -42,9 +42,9 @@ app.use((req, res, next) => {
 });
 
 if (isProduction) {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
   });
 }
 
